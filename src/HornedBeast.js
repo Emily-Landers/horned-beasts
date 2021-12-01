36273459b25dff437c94c48ea412f3db7507d67a
Emily Landers <emily.j.landers@gmail.com>
2021-12-01 11:06:44 -0500

    progress

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
- 
-
+import data from './data.json';
     class HornedBeast extends Component {
-        render(){
-            return(
-                <div>
-                    <h2>{this.props.title}</h2>
-                    <img src={this.props.img_url} alt={this.props.alt} title={this.props.title} />
-
-                    <p>{this.props.description}</p> 
-
-                </div>
+        constructor(props) {
+            super(props);
+            this.state = {
+              votes: 0,
+              beast: {}
+              beastData: data
+            }
+          }
+        
+          handleClick = () => {
+            this.setState({ votes: this.state.votes + 1 });
+          }
+        
+          render() {
+            return (
+              <div>
+                <img src={this.props.img_url} alt={this.props.alt} title={this.props.title}/>
+                <h3>{this.props.name}</h3>
+                <p>{this.props.description}</p>
+                <p>Number of votes: {this.state.votes}</p>
+                <button onClick={this.handleClick}>Vote</button>
+              </div>
             )
-            }    
-    }
-
+          }
+        }
     
     export default HornedBeast;
+    
--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,35 @@
 import { Component } from 'react';
 import HornedBeast from './HornedBeast'
 
+
 class Main extends Component {
   render() {
+    data.map(beast => { 
+     
+
+    } )
     return (
   <div>
-    <HornedBeast title="Water Buffalo" img_url="https://upload.wikimedia.org/wikipedia/commons/c/cf/Serengeti_Bueffel2.jpg" description="Mustache head"/>
-    <HornedBeast title="Narwhal" img_url="https://previews.123rf.com/images/lineartestpilot/lineartestpilot1909/lineartestpilot190907578/129368587-rainbow-gradient-line-drawing-of-a-cartoon-narwhal.jpg" description="spooky narwhal"/>
+    {/* <HornedBeast title="UniWhal" image_url = "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"/>
+    <HornedBeast title="UniWhal" image_url = "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"/>
+    <HornedBeast title="UniWhal" image_url = "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"/>
+    <HornedBeast title="UniWhal" image_url = "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"/>
+    <HornedBeast title="UniWhal" image_url = "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"/>
+    <HornedBeast title="UniWhal" image_url = "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"/>
+    <HornedBeast title="UniWhal" image_url = "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"/>
+    <HornedBeast title="UniWhal" image_url = "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"/>
+    <HornedBeast title="UniWhal" image_url = "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"/>
+    <HornedBeast title="UniWhal" image_url = "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"/>
+    <HornedBeast title="UniWhal" image_url = "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"/>
+    <HornedBeast title="UniWhal" image_url = "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"/>
+    <HornedBeast title="UniWhal" image_url = "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"/>
+    <HornedBeast title="UniWhal" image_url = "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"/>
+    <HornedBeast title="UniWhal" image_url = "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"/>
+    <HornedBeast title="UniWhal" image_url = "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"/>
+    <HornedBeast title="UniWhal" image_url = "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"/>
+    <HornedBeast title="UniWhal" image_url = "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"/>
+    <HornedBeast title="UniWhal" image_url = "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"/>
+    <HornedBeast title="Narwhal" img_url="https://previews.123rf.com/images/lineartestpilot/lineartestpilot1909/lineartestpilot190907578/129368587-rainbow-gradient-line-drawing-of-a-cartoon-narwhal.jpg" description="spooky narwhal"/> */}
   </div>
     )
   }
